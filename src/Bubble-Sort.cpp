5ca9c9989f9554173cb10a35ab1c5f37e4b25b9a
Eason0484 <spec1al_0484@yeah.net>
2022-02-13 11:38:11 +0800

    Commit

--- src/Bubble-Sort.cpp
+++ src/Bubble-Sort.cpp
@@ -7,9 +7,12 @@ int main()
     int input[n];
     for (i = 0; i < n; i++)
         cin >> input[i];
-    for (i = n - 1; i >= 1; i--) {
-        for (j = 0; j < i; j++) {
-            if (input[j] > input[j + 1]) {
+    for (i = n - 1; i >= 1; i--)
+    {
+        for (j = 0; j < i; j++)
+        {
+            if (input[j] > input[j + 1])
+            {
                 exchanger = input[j];
                 input[j] = input[j + 1];
                 input[j + 1] = exchanger;
