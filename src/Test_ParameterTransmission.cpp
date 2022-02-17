4e3ba1750f3ca17ccf682b58d140b8abe569afa8
Eason0484 <spec1al_0484@yeah.net>
2022-02-17 08:37:50 +0800

    Commit

--- src/Test_ParameterTransmission.cpp
+++ src/Test_ParameterTransmission.cpp
@@ -8,8 +8,9 @@ int main(int argc, char *argv[])
     }
 }
 
-/* argc = argument count, 表示传入main函数的数组元素个数，为int类型，
-   argv = argument vector, 表示传入main函数的指针数组，为char**类型。
-   第一个数组元素argv[0]是程序名称，并且包含程序所在的完整路径。
-   argc至少为1，即argv数组至少包含程序名。
+/*
+ * argc = argument count, 表示传入main函数的数组元素个数，为int类型，
+ * argv = argument vector, 表示传入main函数的指针数组，为char**类型。
+ * 第一个数组元素argv[0]是程序名称，并且包含程序所在的完整路径。
+ * argc至少为1，即argv数组至少包含程序名。
  */
