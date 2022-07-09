3634db8e66aa8e074fd8719f9b5769906dcafff8
Eason0484 <spec1al_0484@yeah.net>
2022-07-09 14:43:45 +0800

    Commit

--- src/BFS_3_Cell.cpp
+++ src/BFS_3_Cell.cpp
@@ -3,33 +3,15 @@
 
 using namespace std;
 
-int a[101];
+int a[101], map[101][101];
 bool v[101];
 queue<int> que;
 void BFS();
 
-int map[9][9] = {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 1, 1},
-    {0, 0, 1, 1, 1, 1, 0, 1, 1},
-    {0, 0, 1, 1, 0, 0, 1, 1, 1},
-    {0, 0, 1, 0, 1, 1, 1, 0, 1},
-    {0, 1, 1, 0, 1, 1, 1, 0, 0},
-    {0, 0, 0, 1, 1, 1, 1, 1, 0},
-    {0, 1, 1, 1, 0, 0, 1, 1, 0},
-    {0, 1, 1, 1, 1, 0, 0, 0, 1},
-};
-
 int main(int argc, char **)
 {
-    int k = 8;
+    using namespace std;
     BFS();
-    cout << char(k + 64);
-    while (a[k])
-    {
-        cout << "-" << char(a[k] + 64);
-        k = a[k];
-    }
     cout << "\n请按任意键继续...";
     cin.clear();
     cin.sync();
