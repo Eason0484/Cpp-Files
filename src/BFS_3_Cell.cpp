715fa16b36393ef8a6f2a7305799e5776df33502
Eason0484 <spec1al_0484@yeah.net>
2022-07-09 15:23:02 +0800

    Commit

--- src/BFS_3_Cell.cpp
+++ src/BFS_3_Cell.cpp
@@ -7,69 +7,68 @@ struct Node
     int y;
     int step;
 };
-int map[101][101];
-bool v[101][101];
+int a[101][101];
+bool book[101][101];
+char s[101];
 
-int n, m;
-int sx, sy, ex, ey;
-bool flag = false;
+int n, m, ans;
 int np[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-queue<Node> que;
-void BFS();
+
+void BFS(int x, int y);
+
 int main()
 {
     cin >> n >> m;
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= m; j++)
+        cin.getline(s, 101);
+        for (int j = 0; j < m; j++)
         {
-            cin >> map[i][j];
+            if (s[j] != '0')
+            {
+                a[i][j] = 1;
+            }
         }
     }
-    cin >> sx >> sy >> ex >> ey;
-    BFS();
-    if (flag)
+    for (int i = 0; i < n; i++)
     {
-        cout << que.back().step << endl;
+        for (int j = 0; j < m; j++)
+        {
+            if (a[i][j] == 1 && !book[i])
+            {
+                BFS(i, j);
+            }
+        }
     }
-    else
-    {
-        cout << -1 << endl;
-    }
-    cout << "请按任意键继续...";
+    cout << ans << "\n请按任意键继续...";
     cin.clear();
     cin.sync();
     cin.get();
     return 0;
 }
 
-void BFS()
+void BFS(int x, int y)
 {
+    queue<Node> que;
     Node sn;
-    sn.x = sx;
-    sn.y = sy;
-    sn.step = 0;
-    v[sx][sy] = true;
+    sn.x = x;
+    sn.y = y;
+    book[x][y] = true;
     que.push(sn);
+    ans++;
     while (!que.empty())
     {
         for (int i = 0; i < 4; i++)
         {
             int nx = que.front().x + np[i][0];
             int ny = que.front().y + np[i][1];
-            if (nx >= 1 && nx <= n && ny >= 1 && ny <= m && v[nx][ny] == 0 && !v[nx][ny])
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && a[nx][ny] == 1 && !book[nx][ny])
             {
-                v[nx][ny] = true;
-                Node newnode;
-                newnode.x = nx;
-                newnode.y = ny;
-                newnode.step = que.front().step + 1;
-                que.push(newnode);
-            }
-            if (nx == ex && ny == ey)
-            {
-                flag = true;
-                return;
+                book[nx][ny] = true;
+                Node newNode;
+                newNode.x = nx;
+                newNode.y = ny;
+                que.push(newNode);
             }
         }
         que.pop();
