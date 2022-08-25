d4655f6ab945fb62a2ef2554d8ee842f8a3a126e
Eason0484 <spec1al_0484@yeah.net>
2022-08-25 18:05:25 +0800

    Commit

--- src/DP_10_PBP_Better.cpp
+++ src/DP_10_PBP_Better.cpp
@@ -7,32 +7,32 @@ void Solution();
 
 int main()
 {
-	using namespace std;
-	cin >> v >> itmCnt;
-	for (int i = 1; i <= itmCnt; i++)
-	{
-		cin >> wt[i] >> vl[i];
-	}
-	Solution();
-	return 0;
+    using namespace std;
+    cin >> v >> itmCnt;
+    for (int i = 1; i <= itmCnt; i++)
+    {
+        cin >> wt[i] >> vl[i];
+    }
+    Solution();
+    return 0;
 }
 
 void Solution()
 {
-	using namespace std;
-	for (int i = 1; i <= itmCnt; i++)
-	{
-		for (int j = wt[i]; j <= v; j++)
-		{
-			dp[j] = max(dp[j], dp[j - wt[i]] + vl[i]);
-		}
-	}
-	cout << "dp[i]:\n";
-	for (int i = 1; i <= v; i++)
-	{
-		cout << dp[i] << " ";
-	}
-	cout << "\nAnswer: " << dp[v] << endl;
+    using namespace std;
+    for (int i = 1; i <= itmCnt; i++)
+    {
+        for (int j = wt[i]; j <= v; j++)
+        {
+            dp[j] = max(dp[j], dp[j - wt[i]] + vl[i]);
+        }
+    }
+    cout << "dp[i]:\n";
+    for (int i = 1; i <= v; i++)
+    {
+        cout << dp[i] << " ";
+    }
+    cout << "\nAnswer: " << dp[v] << endl;
 }
 
 /*
